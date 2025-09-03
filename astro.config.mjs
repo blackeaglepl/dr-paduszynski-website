f1ec1abe9f6a7a066041a95b73f84b093bdd59bf
blackeaglepl <user@example.com>
2025-09-03 09:57:05 +0200

    wersja 1.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://osteopatia-paduszynski.pl/',
-  trailingSlash: 'always',
+  trailingSlash: 'ignore', // Changed from 'always' - fixes mobile vs desktop routing issues
   build: {
     format: 'directory',
   },
